d5b14e84df368448a39e47acf700ed5f40777b47
craftsmon <craftsmon@outlook.com>
2017-09-09 13:13:41 +0300

    Configuration file for sequelize-cli and generated folder structure ./{models,seeders,migrations,config.json}

---
+++ server/models/user.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type:DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+  User.associate = (models) => {
+
+  }
+  return User;
+};
