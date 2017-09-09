ac0566afce9b30ca9736b8553feaac2b72e454f0
craftsmon <craftsmon@outlook.com>
2017-09-09 15:49:58 +0300

    Route for registering a new user

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 const router = require("express").Router();
+const Controller = require("../controllers/controller");
 
-router.get("/", (req, res, next) => {
-  res.render("index");
+router.get("/", (req, res) => {
+  // res.render("index");
+  res.send(req.body);
 });
-
+router.get("/register", (req, res) => {
+  res.send("Registration page");
+});
+router.post("/register", Controller.users.add);
+router.get("/login");
 
 module.exports = router;
